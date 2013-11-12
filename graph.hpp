59bcfb660a9f9cc6193b20dbbc05225bd588c9cd
Jacob Stevenson <js850@cam.ac.uk>
2013-11-12 18:15:09 +0000

    use node_id typedef throughout
    add some documentation

--- graph.hpp
+++ graph.hpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <list>
+#include <map>
 
 namespace graph_ns
 {
@@ -17,21 +18,27 @@ color_type color_white = 0;
 color_type color_grey = 1;
 color_type color_black = 4;
 
+/**
+ * basic class for a node in the graph
+ */
 class Node{
 public:
-    size_t id_;
+    node_id id_;
     Edge * first_outgoing_; // first outgoing edge
 
-    Node(size_t id):
+    Node(node_id id):
         id_(id),
         first_outgoing_(NULL)
     {}
 
     void add_out_edge(Edge * edge);
     Edge * first_out_edge(){ return first_outgoing_; }
-    size_t id() const { return id_; }
+    node_id id() const { return id_; }
 };
 
+/**
+ * basic class for an edge (arc) in the graph
+ */
 class Edge{
 public:
     Node * head_; // node the edge points to
@@ -61,7 +68,7 @@ class Graph
     std::list<Node *> node_list_;
     std::list<Edge *> edge_list_;
 
-    size_t next_node_id_;
+    node_id next_node_id_;
 
 public:
     Graph():
@@ -70,6 +77,7 @@ public:
 
     ~Graph()
     {
+        // delete all the nodes and edges
         for (std::list<Node *>::iterator iter = node_list_.begin();
                 iter != node_list_.end(); ++iter){
             delete *iter;
@@ -80,28 +88,32 @@ public:
         }
     }
 
-    size_t number_of_nodes() { return node_list_.size(); }
-    size_t number_of_edges() { return edge_list_.size(); }
+    node_id number_of_nodes() { return node_list_.size(); }
+    node_id number_of_edges() { return edge_list_.size(); }
 
     /**
      * create a new node
      */
-    size_t add_node(){
+    node_id add_node(){
         Node *node = new Node(next_node_id_);
         next_node_id_++;
         node_list_.push_back(node);
         return node->id_;
     }
-    void add_nodes(size_t n){
-        for (size_t i = 0; i < n; ++i){
+
+    /**
+     * create a n new nodes
+     */
+    void add_nodes(node_id n){
+        for (node_id i = 0; i < n; ++i){
             add_node();
         }
     }
 
     /**
-     * return the node with given node id
+     * return a pointer to the node with given node id
      */
-    Node * get_node(size_t node_id)
+    Node * get_node(node_id node_id)
     {
         for (std::list<Node *>::iterator iter = node_list_.begin();
                 iter != node_list_.end(); ++iter){
@@ -115,7 +127,7 @@ public:
     /**
      * add an edge from tail to head
      */
-    void add_edge(size_t tail, size_t head)
+    void add_edge(node_id tail, node_id head)
     {
         Node * node_tail = get_node(tail);
         Node * node_head = get_node(head);
